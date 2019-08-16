7f4a288918d2ac33bfe353dbe49742fb044a11c6
general <generals.space@gmail.com>
2019-08-16 19:28:17 +0800

    更新

--- libs/lib.sh
+++ libs/lib.sh
@@ -6,14 +6,3 @@ __file__=$0
 __dir__=$(cd $(dirname $0) && pwd)
 ## 当前用户名
 __user__=$(whoami)
-
-__get_formatted_date() {
-    date +"%Y/%m/%d %H:%M:%S"
-}
-
-## 经过格式化的当前时间, 使用$__now__即可得到
-## 被注释掉的这一行, $__now__得到的是带有引号的字符串, 感觉不太友好.
-## 更换成使用函数生成的字符串
-## __now__='date +"%Y/%m/%d-%H:%M:%S"'
-## __now__是函数, 是函数, 是函数
-__now__=__get_formatted_date
