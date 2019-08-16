9e2b0d8279c7f7d1776864533c459895d445f5d6
general <generals.space@gmail.com>
2019-08-16 09:49:07 +0800

    更新

--- libs/lib.sh
+++ libs/lib.sh
@@ -7,21 +7,8 @@ __dir__=$(cd $(dirname $0) && pwd)
 ## 当前用户名
 __user__=$(whoami)
 
-## __strlen: 计算指定字符串的长度
-##__strlen() {}
-
 __get_formatted_date() {
     date +"%Y/%m/%d %H:%M:%S"
-}
-__strlen() {
-    local str=$1
-    ## 1. wc 的-L选项, 直接获取当前行的字符数
-    echo $1 | wc -L
-    ## 2. ${}方法, 把字符串变量当作是数组处理
-    ## echo ${#str}
-    ## 3. awk内置的length()函数
-    ## echo $a | awk '{print length($0)}'
-    ## 4. expr内置lenght无法处理变量中存在空格的情形, 放弃使用
 }
 
 ## 经过格式化的当前时间, 使用$__now__即可得到
